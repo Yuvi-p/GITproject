d91df1a2d2b10c61ffbe8e81e1af7bda24230094
Yuvi-p <189519015+Yuvi-p@users.noreply.github.com>
2026-04-26 12:19:03 +0300

    Added ex3-yuval files to the main project

---
+++ ex3-yuval/yuval.java
@@ -0,0 +1,7 @@
+public class yuval {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("My name is Yuval Pery - Iteration " + i);
+        }
+    }
+}
